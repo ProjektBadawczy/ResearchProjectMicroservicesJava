e37f663787153c75ecf3e650bcb88eb62a339916
Weronika <weronika.piotrowska.pl@gmail.com>
2023-05-26 10:17:51 +0200

    Change client for requests

--- EdmondsKarpService/src/main/java/studies/research/project/microservicces/edmondskarpservice/service/EdmondsKarpService.java
+++ EdmondsKarpService/src/main/java/studies/research/project/microservicces/edmondskarpservice/service/EdmondsKarpService.java
@@ -2,6 +2,9 @@ package studies.research.project.microservicces.edmondskarpservice.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import studies.research.project.microservicces.edmondskarpservice.model.*;
 
 import static java.lang.Math.min;
@@ -14,14 +17,38 @@ public class EdmondsKarpService {
     }
 
     public int calculateMaxFlow(int id, int source, int destination) {
-        String graphServiceUrl = "http://graph-service:80/graph?id=".concat(Integer.toString(id));
-        Graph graph = new RestTemplate().getForEntity(graphServiceUrl, Graph.class).getBody();
+        Mono<Graph> graphMono = WebClient.create("http://graph-service:80")
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/graph")
+                        .queryParam("id", Integer.toString(id))
+                        .build())
+                .retrieve()
+                .bodyToMono(Graph.class);
+
+
+        Graph graph = graphMono.block();
+
+        //String graphServiceUrl = "http://graph-service:80/graph?id=".concat(Integer.toString(id));
+        //Graph graph = new RestTemplate().getForEntity(graphServiceUrl, Graph.class).getBody();
         if(graph != null){
             int u, v;
             Graph residualGraph = graph.clone();
             int max_flow = 0;
-            String bfsServiceUrl = "http://bfs-service:80/bfs";
-            BFSResult bfsResult = new RestTemplate().postForObject(bfsServiceUrl, new BFSRequest(residualGraph, source, destination), BFSResult.class);
+
+            Mono<BFSResult> bfsResultMono = WebClient.create("http://bfs-service:80")
+                    .post()
+                    .uri(uriBuilder -> uriBuilder.path("/bfs")
+                            .build())
+                    .body(BodyInserters.fromValue(new BFSRequest(residualGraph, source, destination)))
+                    .retrieve()
+                    .bodyToMono(BFSResult.class);
+
+
+            //String bfsServiceUrl = "http://bfs-service:80/bfs";
+            //BFSResult bfsResult = new RestTemplate().postForObject(bfsServiceUrl, new BFSRequest(residualGraph, source, destination), BFSResult.class);
+
+            BFSResult bfsResult = bfsResultMono.block();
+
             while (bfsResult.success()) {
                 int path_flow = Integer.MAX_VALUE;
                 for (v = destination; v != source; v = bfsResult.parents()[v]) {
@@ -36,7 +63,15 @@ public class EdmondsKarpService {
                 }
 
                 max_flow += path_flow;
-                bfsResult = new RestTemplate().postForObject(bfsServiceUrl, new BFSRequest(residualGraph, source, destination), BFSResult.class);
+                //bfsResult = new RestTemplate().postForObject(bfsServiceUrl, new BFSRequest(residualGraph, source, destination), BFSResult.class);
+                bfsResultMono = WebClient.create("http://bfs-service:80")
+                        .post()
+                        .uri(uriBuilder -> uriBuilder.path("/bfs")
+                                .build())
+                        .body(BodyInserters.fromValue(new BFSRequest(residualGraph, source, destination)))
+                        .retrieve()
+                        .bodyToMono(BFSResult.class);
+                bfsResult = bfsResultMono.block();
             }
 
             return max_flow;

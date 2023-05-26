2e74fa5f8a3b9eed23d78b87d4c36ce552bf57a5
Weronika <weronika.piotrowska.pl@gmail.com>
2023-05-26 11:40:47 +0200

    Revert "added async"
    This reverts commit 596d36d09500b643e30af815185447aa0b86320d.

--- BFSService/src/main/java/studies/research/project/microservicces/BFSService/BfsServiceApplication.java
+++ BFSService/src/main/java/studies/research/project/microservicces/BFSService/BfsServiceApplication.java
@@ -2,29 +2,12 @@ package studies.research.project.microservicces.bfsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @SpringBootApplication
-@EnableAsync
 public class BfsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BfsServiceApplication.class, args);
-	}
-
-	@Bean
-	public Executor taskExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(2);
-		executor.setMaxPoolSize(2);
-		executor.setQueueCapacity(500);
-		executor.setThreadNamePrefix("GithubLookup-");
-		executor.initialize();
-		return executor;
 	}
 
 }
--- BFSService/src/main/java/studies/research/project/microservicces/BFSService/controller/BfsController.java
+++ BFSService/src/main/java/studies/research/project/microservicces/BFSService/controller/BfsController.java
@@ -1,12 +1,9 @@
 package studies.research.project.microservicces.bfsservice.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 import studies.research.project.microservicces.bfsservice.model.*;
 import studies.research.project.microservicces.bfsservice.service.BfsService;
-
-import java.util.concurrent.CompletableFuture;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -20,8 +17,7 @@ public class BfsController {
     }
 
     @PostMapping("/bfs")
-    @Async
-    public CompletableFuture<BFSResult> getDirectedGraph(@RequestBody BFSRequest request) {
-        return CompletableFuture.completedFuture(bfsService.bfs(request.graph(), request.source(), request.destination()));
+    public ResponseEntity<BFSResult> getDirectedGraph(@RequestBody BFSRequest request) {
+        return new ResponseEntity<>(bfsService.bfs(request.graph(), request.source(), request.destination()), OK);
     }
 }
